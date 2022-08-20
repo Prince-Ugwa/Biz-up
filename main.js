009e0788314de31eb07b67b65791bd4994484dbf
Prince-Ugwa <odiniyaprinceugwa@gmail.com>
2022-08-20 20:21:07 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const p1 = ["p>E", "E>R", "R>U"];
+
+const p2 = p1.join("").replaceAll(">", "");
+
+const peru = new Set(p2);
+
+const s = ["i>n", "a>i", "p>a", "s>p"];
+const s1 = s.join("").replaceAll(">", "").toUpperCase();
+const s2 = new Set(s1);
+const s3 = [...s2].reverse();
+[s3[3], s3[4]] = [s3[4], s3[3]];
+console.log([...peru].join("").toUpperCase(), s3);
+
+// console.log(p3.toUpperCase());
