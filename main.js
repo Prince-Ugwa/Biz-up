90354f3ef55aba7bfa01eba51a54ca7142c38132
Prince-Ugwa <odiniyaprinceugwa@gmail.com>
2022-08-22 16:46:34 +0100

    commit

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-const p1 = ["p>E", "E>R", "R>U"];
+// const p1 = ["p>E", "E>R", "R>U"];
 
-const p2 = p1.join("").replaceAll(">", "");
+// const p2 = p1.join("").replaceAll(">", "");
 
-const peru = new Set(p2);
+// const peru = new Set(p2);
 
-const s = ["i>n", "a>i", "p>a", "s>p"];
-const s1 = s.join("").replaceAll(">", "").toUpperCase();
-const s2 = new Set(s1);
-const s3 = [...s2].reverse();
-[s3[3], s3[4]] = [s3[4], s3[3]];
-console.log([...peru].join("").toUpperCase(), s3);
+// const s = ["i>n", "a>i", "p>a", "s>p"];
+// const s1 = s.join("").replaceAll(">", "").toUpperCase();
+// const s2 = new Set(s1);
+// const s3 = [...s2].reverse();
+// [s3[3], s3[4]] = [s3[4], s3[3]];
+// console.log([...peru].join("").toUpperCase(), s3);
 
-// console.log(p3.toUpperCase());
+// // console.log(p3.toUpperCase());
+
+const header = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("window-scroll", window.scrollY > 50);
+});
